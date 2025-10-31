ac9f2aa5b127008929c7e12709cca09d2e80d50c
Johnkespitia <jcespitia1@gmail.com>
2025-10-30 20:20:09 -0500

    fix: ajustar configuración de scripts y modo en Vite para desarrollo y producción

--- vite.config.js
+++ vite.config.js
@@ -2,24 +2,26 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
-export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/IntecapHalloweenReact/' : '/',
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.js',
-    include: ['src/**/*.spec.{js,ts,jsx,tsx}'],
-    transformMode: {
-      // This forces files matching these regexes to be processed 
-      // by the web transformation pipeline, which handles JSX.
-      web: [/\.[jt]sx$/], 
+export default defineConfig(({mode}) => {
+  const isProd = mode === 'production'
+  return  {
+    base: isProd == true ? '/IntecapHalloweenReact/' : '/',
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './src/setupTests.js',
+      include: ['src/**/*.spec.{js,ts,jsx,tsx}'],
+      transformMode: {
+        // This forces files matching these regexes to be processed 
+        // by the web transformation pipeline, which handles JSX.
+        web: [/\.[jt]sx$/], 
+      },
+      coverage: {
+        reporter: ['text', 'json', 'html'],
+      },
     },
-    coverage: {
-      reporter: ['text', 'json', 'html'],
-    },
-  },
-})
+}})
